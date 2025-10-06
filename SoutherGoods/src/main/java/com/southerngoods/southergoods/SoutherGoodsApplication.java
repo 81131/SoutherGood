e298923694a924c893d3f2b4b7131846c004cfa4
81131 <115885164+81131@users.noreply.github.com>
2025-10-06 18:18:10 +0530

    Add files via upload

---
+++ SoutherGoods/src/main/java/com/southerngoods/southergoods/SoutherGoodsApplication.java
@@ -0,0 +1,13 @@
+package com.southerngoods.southergoods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoutherGoodsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoutherGoodsApplication.class, args);
+    }
+
+}
